a1c1f6c82d218cfe077af19167af9664a73bd724
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-10-11 13:05:54 +0200

    fetshing user data in fetch-data.ja file

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded ',function(){
+document.addEventListener('DOMContentLoaded',function(){
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback')
-   
-    const username  = document.getElementById('username').value.trim();
-    const email  = document.getElementById('email').value.trim();
-    const password  = document.getElementById('password').value.trim();
-    let isValid = true;
-    let messages = [] 
     
     form.addEventListener('submit', function(event) {
+        const username  = document.getElementById('username').value.trim();
+        const email  = document.getElementById('email').value.trim();
+        const password  = document.getElementById('password').value.trim();
+        let isValid = true;
+        let messages = [] 
+
         event.preventDefault();  
         if(username.length < 3){
             isValid = false
             messages.push('username must be more than 3 caracters')
         }
 
-        if(!email.include("@") || !email.include('.')){
+        if(!email.includes("@") || !email.includes('.')){
             isValid = false
             messages.push('invalid email adress')
         }
