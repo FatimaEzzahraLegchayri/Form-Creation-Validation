1532914bc853416e18632938ee42435a38cdbe40
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-10-11 14:59:15 +0200

    fix some bug in fetshing

--- fetch-data.js
+++ fetch-data.js
@@ -2,19 +2,23 @@ async function fetchUserData(){
     const apiUrl  = 'https://jsonplaceholder.typicode.com/users'
     const dataContainer = document.getElementById('api-data')
     try {
-        const response = await fetch.apiUrl
+        const response = await fetch(apiUrl)
         const users = await response.json()
-        console.log(users,'vv');
+        // console.log(users,'vv');
         
         dataContainer.innerHTML = ''
-        const userList = document.createElement('<ul>')
-        users.foreach(e=>{
-            const li = document.createElement('<li>')
-            li.textContent = users.name
+        const userList = document.createElement('ul')
+        
+        users.forEach(e=>{
+            const li = document.createElement('li')
+            li.textContent = e.name
+            console.log(',,,',li);
+            userList.appendChild(li)
         })
-        dataContainer.append('<li>')
+        dataContainer.appendChild(userList)
     
     } catch (error) {
+        dataContainer.textContent = ''        
         dataContainer.textContent = 'Failed to load user data.'        
     }
 }
